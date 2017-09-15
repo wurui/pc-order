7d918d904b583784d0fbb989b37bb1444bebb2c8
wurui <wurui.wr@gmail.com>
2017-09-15 12:47:29 +0800

    optimize

--- asset/index.js
+++ asset/index.js
@@ -1,5 +1,18 @@
-define(['mustache','oxjs'],function(Mustache,OXJS){
-var tpl,$list,customizeRest,orderRest;
+define(['mustache', 'oxjs'], function (Mustache, OXJS) {
+    var tpl, $list, customizeRest, orderRest, searchForm, $totalCount;
+    var StatusCodes = {
+        NEW: 0,
+        UNPAID: 11,
+        PAID: 1,
+        RECEIVED: 2,
+        DELIVERED: 3,
+        COMPLETED: 4,
+        CLOSED: 5,
+        REFUNDED: 8,
+        REFUND: 9,
+        REFUNDING: 91,
+        REFUND_FAIL: 94
+    };
     var timeformat = function (d) {
         if (typeof d != 'object') {
             d = new Date(d);
@@ -17,158 +30,35 @@ var tpl,$list,customizeRest,orderRest;
         }
         return [this.name, '(' + this.phone + ')', this.province, this.city, this.district, this.detail].join(' ')
     }
-    var statusDesc = function (data) {
-        var st = data.status;
-        switch (st) {
-            case -1:
-                return '<font color="#999">订单已关闭</font>';
-            case 0:
-                return '<font color="#f60">待付款</font>';
-            case 0.1:
-                return '<font color="#f60">激活时付款</font>';
-            case 1:
-                return '<font color="#666">已付款</font>';
-            case 2:
-                return '<font color="#666">生产中</font>';
-            case 3:
-                return '<font color="#060">已发货</font>';
-            case 4:
-                return '<font>订单完成</font>';
-            case 8:
-                return '<font color="#999">已退款</font>'
-            case 9.4:
-                return '<font color="#f10">退款失败</font>'
-            case 9.1:
-                return '<font color="#666">退款处理中</font>'
-            case 9:
-                return '<font color="#666">退款中</font>'
-            default :
-                return ''
-        }
-    };
-    var orderOP = function (data) {
-        var st = data.status;
-        switch (st) {
-            case 0:
-                return '<button data-role="close" type="button">关闭订单</button>&nbsp;&nbsp;&nbsp;&nbsp;';
-            case 1:
-                return '<button data-role="refund" type="button">退款</button>';
-            case 3:
-                return '<button data-role="wuliu" data-no="' + data.delivery_no + '">查看物流</button>';
-            case 2:
-            case 4:
-            case 8:
-                return ''
-            case 9.4:
-                return '退款失败,请等待客服处理'
-            case 9.1:
-                return data.reason == 'h5_none' ? '' : '退款理由:' + data.reason
-            case 9:
-                return (data.reason == 'h5_none' ? '' : '退款理由:' + data.reason) + '&nbsp;&nbsp;&nbsp;&nbsp;<button data-role="cancelrefund" type="button">撤消退款</button>'
-            default :
-                return '<button data-role="del" type="button">删除订单</button>'
-        }
-    };
-    var showWuliu = function ($tar) {
 
-
-    };
-    var orderStatus=function (data) {
+    var orderStatus = function (data) {
         var st = data.status;
         switch (st) {
-            case 0:
-                return '<p>未付款</p><button data-role="close" type="button">关闭订单</button>';
-            case 0.1:
+            case StatusCodes.NEW:
+                return '<p>未付款</p>';//<button data-role="close" type="button">关闭</button>
+            case StatusCodes.UNPAID:
                 return '<p>激活时付款</p><button data-role="produce" type="button">生产</button>'
-            case 1://todo: 后面去掉单独生产
+            case StatusCodes.PAID://todo: 后面去掉单独生产
                 return '<p>已付款</p><button data-role="produce" type="button">生产</button>';
-            case 2:
-                return '<p>生产中</p><button data-role="send" type="button">发货</button><br/><br/><button data-role="produce" type="button">重新生产</button><br/><br/><a href="/smctadmin/exportsheet?oids='+data._id+'" target="download">导出发货单</a>';
-            case 3:
+            case StatusCodes.RECEIVED:
+                return '<p>生产中</p><button data-role="send" type="button">发货</button><br/><br/><button data-role="produce" type="button">重新生产</button><br/><br/><a href="/smctadmin/exportsheet?oids=' + data._id + '" target="download">导出发货单</a>';
+            case StatusCodes.DELIVERED:
                 return '<p>已发货</p><p>' + data.delivery_no + '</p><p><button data-role="wuliu" data-no="' + data.delivery_no + '">查看物流</button></p>';
-            case 4:
-                return '<p>订单完成</p><br/>'+Util.timeformat(data.mts);
-            case 9.4:
+            case StatusCodes.COMPLETED:
+                return '<p>订单完成</p><br/>' + timeformat(data.mts);
+            case StatusCodes.REFUND_FAIL:
                 return '<p>退款失败</p><p><button data-role="force" type="button">手工处理</button></p>'
-            case 8:
+            case StatusCodes.REFUNDED:
                 return '<p>退款完成</p>'
-            case 9.1:
-            case 9:
-                return '<p>买家申请退款<br/>时间:' + Util.timeformat(data.mts) + '<br/>理由:' + data.reason + '</p><a target="_blank" href="/smctadmin/dealrefund?from=smct&oid=' + data._id + '">' + (st == 9.1 ? '继续' : '') + '退款</a><p><button data-role="force" type="button">手工处理</button></p>'
+            case StatusCodes.REFUND:
+            case StatusCodes.REFUNDING:
+                return '<p>买家申请退款<br/>时间:' + timeformat(data.mts) + '</p><a target="_blank" href="/smctadmin/dealrefund?from=smct&oid=' + data._id + '">' + (st == StatusCodes.REFUNDING ? '继续' : '') + '退款</a><p><button data-role="force" type="button">手工处理</button></p>'
+            case StatusCodes.CLOSED:
+                return '<p>订单已关闭</p>'//<button data-role="del" type="button">删除订单</button>
             default :
-                return '<p>--</p><button data-role="del" type="button">删除订单</button>'
+                return '<p>--</p>'
         }
     }
-
-    var getAndRender = function () {
-
-        orderRest.get(function (r) {
-            // $.getJSON(apiHost + '/smct/getorders?callback=?', function (r) {
-            if (r && r.length) {
-                var list = r;
-                var bids = [];
-                //var totalfee = 0;
-                for (var i = 0, n; n = list[i++];) {
-                    //n.order_no= n._id.toUpperCase();
-                    //n.order_time= (new Date(n.cts)).toLocaleString();
-                    var pack = n.pack;
-                    if (pack && pack.length) {
-                        for (var j = 0; j < pack.length; j++) {
-                            var packitem = pack[j];
-                            bids.push(packitem.customize);
-                        }
-                    }
-                }
-                customizeRest.get({ids: bids.join(',')}, function (r) {
-
-                    // $.getJSON(apiHost + '/smct/getbuilds?bids=' + bids.join(',') + '&callback=?', function (r) {
-                    var buildObj = {}, builds = r;
-                    for (var i = 0, build; build = builds[i++];) {
-                        buildObj[build._id] = build;
-                        //console.log(build._id)
-                    }
-
-
-                    for (var i = 0, n; n = list[i++];) {
-                        n.order_no = n._id.toUpperCase();
-                        n.order_time = n.cts?timeformat(n.cts): n.time;
-                        n.address = addrToString.call(n.delivery)
-                        //n.status='已付款';
-                        n.op = orderOP(n);
-                        n.statusDesc = statusDesc(n)
-                        n.totalsum = (n.totalfee - 0).toFixed(2);
-                        if (n.pack && n.pack.length) {
-                            for (var j = 0, pack; pack = n.pack[j]; j++) {
-
-                                pack.setting = buildObj[pack.customize] && param2settings(buildObj[pack.customize].props)
-
-                                //console.log(pack.setting)
-                            }
-                        }
-                    }
-
-                    $list.html(Mustache.render(tpl, {
-                        data: list,
-                        fullcarlogo: function () {
-
-                            var str = ''
-                            if (/\d+/.test(this)) {
-                                str = 'cars/' + this + '.png'
-                            } else {
-                                str = 'carlogo/' + this + '.jpg'
-                            }
-                            return 'http://v.oxm1.cc/' + str
-                        }
-                    }));
-
-                });
-
-            } else {
-                $list.html('<i class="iconfont">&#xe631;</i>&nbsp;&nbsp;<br/>暂无订单,赶紧去定制一个你喜欢的车贴吧~<br/><a href="' + buildurl + '">开始定制 &raquo;</a><br/><br/>').addClass('empty-order');
-
-            }
-        });
-    };
 
     var param2settings = function (param) {
         if (!param)return {};
@@ -179,12 +69,45 @@ var tpl,$list,customizeRest,orderRest;
         return obj;
     };
 
-var getAndRender = function () {
+    var getAndRender = function () {
 
-        orderRest.get({seller:1},function (r) {
+        var sf = searchForm;
+        var status = sf.status.value;
+        var days = sf.days.value;
+        var _id = sf._id.value;
+        var s = {seller:1};
+        if (days) {
+            var oneday = 24 * 3600 * 1000,
+                now = new Date;
+            now.setHours(8 + now.getTimezoneOffset() / 60, 0, 0, 0);
+            var ts0 = now.getTime() - days * oneday;
+            s._cts = ts0;//{$gte: ts0};
+
+        }
+        if (status) {
+
+
+            switch (status) {
+                case 'admin':
+                    s.status = [StatusCodes.PAID,StatusCodes.RECEIVED,StatusCodes.REFUND,StatusCodes.REFUNDING,StatusCodes.REFUND_FAIL].join(',');
+                    break
+                case 'produce':
+                    s.status = StatusCodes.PAID;
+                    break
+                default :
+                    s.status = status - 0;
+                    break
+            }
+        }
+        if (_id) {
+            s._id = _id.toLowerCase();
+        }
+
+        orderRest.get(s, function (r) {
             // $.getJSON(apiHost + '/smct/getorders?callback=?', function (r) {
             if (r && r.length) {
                 var list = r;
+                $totalCount.html(list.length)
                 var bids = [];
                 //var totalfee = 0;
                 for (var i = 0, n; n = list[i++];) {
@@ -199,29 +122,23 @@ var getAndRender = function () {
                     }
                 }
                 customizeRest.get({ids: bids.join(',')}, function (r) {
-
-                    // $.getJSON(apiHost + '/smct/getbuilds?bids=' + bids.join(',') + '&callback=?', function (r) {
                     var buildObj = {}, builds = r;
                     for (var i = 0, build; build = builds[i++];) {
                         buildObj[build._id] = build;
-                        //console.log(build._id)
                     }
 
 
                     for (var i = 0, n; n = list[i++];) {
                         n.order_no = n._id.toUpperCase();
-                        n.order_time = n.cts?timeformat(n.cts): n.time;
+                        n.order_time = n.cts ? timeformat(n.cts) : n.time;
                         n.address = addrToString.call(n.delivery)
                         //n.status='已付款';
                         n.op = orderStatus(n);
-                       // n.statusDesc = statusDesc(n)
+                        // n.statusDesc = statusDesc(n)
                         n.totalsum = (n.totalfee - 0).toFixed(2);
                         if (n.pack && n.pack.length) {
                             for (var j = 0, pack; pack = n.pack[j]; j++) {
-
                                 pack.setting = buildObj[pack.customize] && param2settings(buildObj[pack.customize].props)
-
-                                //console.log(pack.setting)
                             }
                         }
                     }
@@ -243,23 +160,66 @@ var getAndRender = function () {
                 });
 
             } else {
-                $list.html('<i class="iconfont">&#xe631;</i>&nbsp;&nbsp;<br/>暂无订单,赶紧去定制一个你喜欢的车贴吧~<br/><a href="">开始定制 &raquo;</a><br/><br/>').addClass('empty-order');
-
+                $list.html('<div class="empty-order"><i class="iconfont">&#xe631;</i>&nbsp;&nbsp;<br/>该查询条件下暂无订单~<br/><br/><br/></div>');
+                $totalCount.html(0)
             }
         });
     };
-  return {
-    init:function($mod){
-        $list=$('.J_list',$mod);
-        tpl=$('.J_tpl',$mod).html();
-        var uid = $mod.attr('data-uid'),
-            dsid=$mod.attr('data-dsid');
-        //payurl=$mod.attr('data-payurl');
-        customizeRest = OXJS.useREST('customize/'+dsid+'/u/' + encodeURIComponent(uid)).setDevHost('http://local.openxsl.com/'),
-        orderRest = OXJS.useREST('order/'+dsid+'/u/' + encodeURIComponent(uid)).setDevHost('http://local.openxsl.com/');
+    return {
+        init: function ($mod) {
+            $list = $('.J_list', $mod).on('click',function(e){
+                var tar=e.target,
+                    $tb=$(tar).closest('table'),
+                    _id=$tb.attr('data-id')
+                switch (tar.getAttribute('data-role')){
+                    case 'del':
+                        orderRest.del({_id:_id},function(r){
+                            if(r && r.code==0){
+                                $tb.remove();
+                            }
+                        });
+                        break
+                    case 'close':
+                        orderRest.put({_id:_id,status:5},function(r){
+                            if(r && r.code==0){
+                                getAndRender();
+                            }
+                        });
+                        break
+                    case 'produce':
+                        orderRest.put({_id:_id,status:2},function(r){
+                            if(r && r.code==0){
+                                getAndRender();
+                            }
+                        });
+                        break
+                    case 'send':
+                        orderRest.put({_id:_id,status:3,delivery_no:'123'},function(r){
+                            if(r && r.code==0){
+                                getAndRender();
+                            }
+                        });
+                        break
+                    case 'wuliu':
+                        break
+                    case 'force':
+                        break
+                }
+            });
+            tpl = $('.J_tpl', $mod).html();
+            searchForm = $('.J_searchForm', $mod).on('change',getAndRender)[0];
+            $totalCount = $('.J_totalCount', $mod);
+            var uid = $mod.attr('data-uid'),
+                dsid = $mod.attr('data-dsid');
+            //payurl=$mod.attr('data-payurl');
+            customizeRest = OXJS.useREST('customize/' + dsid + '/u/' + encodeURIComponent(uid)).setDevHost('http://dev.openxsl.com/'),
+                orderRest = OXJS.useREST('order/' + dsid + '/u/' + encodeURIComponent(uid)).setDevHost('http://dev.openxsl.com/');
 
-        getAndRender();
 
+
+
+            getAndRender();
+
+        }
     }
-  }
 })
--- mod.xsl
+++ mod.xsl
@@ -3,60 +3,141 @@
         <xsl:param name="dsid">e0ee59439b39fcc3</xsl:param>
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-pc-order" ox-mod="pc-order" data-uid="{login/uid}" data-dsid="{$dsid}">
+            <form class="J_searchForm">
+                <div class="searchbar lrbar lrbar37">
+                    <span>
+                        <select name="days">
+                            <option value="0">今天</option>
+                            <option selected="selected" value="7">近7天</option>
+                            <option value="">不限</option>
+                        </select>
+                        &#160;&#160;
+                        <select name="status">
+                            <option value="">不限</option>
+                            <option value="admin">待处理</option>
+                            <option value="produce">待生产</option>
+                            <option value="9">退款</option>
+                            <option value="2">生产中</option>
+                            <option value="3">已发货</option>
+                            <option value="4">已完成</option>
+                        </select>
+
+                        &#160;&#160;
+
+                        <input type="text" placeholder="订单编号" name="_id"/>
+                        &#160;
+                    </span>
+                    <span class="admin-ops">
+                        <button type="button" onclick="batchProduct()" class="bt-produce">
+                            <i class="iconfont">&#xe64c;</i>
+                            生产
+                        </button>
+                        &#160;&#160;&#160;&#160;
+                        <a href="/smct/dealrefund" target="_blank" onclick="return batchRefund(this)" class="bt-refund">
+                            <i class="iconfont">&#xe671;</i>
+                            退款
+                        </a>
+                        &#160;&#160;&#160;&#160;
+                        <a href="" target="download" class="bt-refund" onclick="return batchExport(this)">
+                            <i class="iconfont">&#xe66b;</i>
+                            导出发货单
+                        </a>
+                        &#160;&#160;&#160;&#160;
+                        <label class="bt-refund" for="fileImport">
+                            <i class="iconfont">&#xe607;</i>
+                            导入物流单
+                        </label>
+                        &#160;&#160;&#160;&#160;
+                        <label>
+                            <input type="checkbox" name="checkall"/>
+                            全选
+                        </label>
+                    </span>
+
+                </div>
+            </form>
+
             <div class="J_list"></div>
+            <div class="lrbar">
+                <span>
+                    共<big class="J_totalCount">0</big>条
+                </span>
+                <span class="admin-ops">
+                    批量操作&#160;&#160;
+                    <button type="button" onclick="batchProduct()" class="bt-produce">
+                        <i class="iconfont">&#xe64c;</i>
+                        生产
+                    </button>
+                    &#160;&#160;&#160;&#160;
+                    <a href="/smct/dealrefund" target="_blank" onclick="return batchRefund(this)" class="bt-refund">
+                        <i class="iconfont">&#xe671;</i>
+                        退款
+                    </a>
+                    &#160;&#160;&#160;&#160;
+                    <button type="button" class="bt-refund">
+                        <i class="iconfont">&#xe66b;</i>
+                        导出发货单
+                    </button>
+                    &#160;&#160;&#160;&#160;
+                </span>
+
+            </div>
             <script type="text/tpl" class="J_tpl"><![CDATA[
             {{#data}}
             <table class="orders-table" cellpadding="0" cellspacing="0" data-id={{_id}}>
-            <thead>
+                <thead>
+                    <tr>
+                        <td>
+                            订单编号: {{order_no}}{{#client}}<span class="order-client-{{client}}"></span>{{/client}}&#160;&#160;&#160;{{{source}}}
+                            &#160;&#160;&#160;&#160;&#160;&#160;<i class="iconfont">&#xe615;</i>{{uid}}
+                        </td>
+                        <td colspan="2">
+                            订单时间: {{order_time}}
+                            &#160;&#160;
+                            <label>
+                                <input type="checkbox" class="J_ck" name="order_ck" data-status="{{status}}" value="{{_id}}"/>选择
+                            </label>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
                 <tr>
-                    <td>订单编号: {{order_no}}{{#client}}<span class="order-client-{{client}}"></span>{{/client}}&nbsp;&nbsp;&nbsp;{{{source}}}</td>
                     <td colspan="2">
-                        订单时间: {{order_time}}
-                        &nbsp;&nbsp;
-                        <label>
-                            <input type="checkbox" class="J_ck" name="order_ck" data-status="{{status}}" value="{{_id}}"/>选择
-                        </label>
+                        {{#pack}}
+                        <div class="order-item" data-bid="{{bid}}" data-count="{{amount}}">
+                            <div class="snapshot">
+                                <div class="preview bgcolor-{{setting.bgcolor}}"">
+                                <div class="card-header">{{setting.text1}}</div>
+                                <div class="card-body tpl tpl-{{setting.tpl}}">
+                                    <div class="central">
+                                        {{#setting.carlogo}}<img src="{{fullcarlogo}}"/>{{/setting.carlogo}}
+                                    </div>
+                                    <img src="http://i.oxm1.cc/uploads/git/wurui/img/2ahkwkkveTj1rgh0ueRlcquA5vz-1000.png" class="qrcode"/>
+                                </div>
+                                <div class="card-footer">
+                                    <span>{{setting.text2}}</span>
+                                </div>
+                            </div>
+                        </div>
+                        &times;{{amount}}
+                    </div>
+                    {{/pack}}
+                </td>
+                <td class="status-{{status}} order-op">
+                    {{{op}}}
+                </td>
+
+            </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td>配送地址: {{address}}</td>
+                    <td>总数: {{totalcount}}</td>
+                    <td width=200>
+                        {{{payinfo}}}实付金额:&#160;&#160;<b class="price">{{totalsum}}</b>
                     </td>
                 </tr>
-            </thead>
-            <tbody>
-            <tr>
-            <td colspan="2">
-            {{#pack}}
-            <div class="order-item" data-bid="{{bid}}" data-count="{{amount}}">
-                <div class="snapshot">
-                    <div class="preview bgcolor-{{setting.bgcolor}}"">
-                    <div class="card-header">{{setting.text1}}</div>
-                    <div class="card-body tpl tpl-{{setting.tpl}}">
-                        <div class="central">
-                            {{#setting.carlogo}}<img src="{{fullcarlogo}}"/>{{/setting.carlogo}}
-                        </div>
-                        <img src="http://i.oxm1.cc/uploads/git/wurui/img/2ahkwkkveTj1rgh0ueRlcquA5vz-1000.png" class="qrcode"/>
-                    </div>
-                    <div class="card-footer">
-                        <span>{{setting.text2}}</span>
-                    </div>
-                </div>
-            </div>
-            &times;{{amount}}
-        </div>
-        {{/pack}}
-    </td>
-    <td>
-        {{{op}}}
-    </td>
-
-</tr>
-        </tbody>
-<tfoot>
-<tr>
-    <td>配送地址: {{address}}</td>
-    <td>总数: {{totalcount}}</td>
-    <td width=200>
-    {{{payinfo}}}实付金额:&nbsp;&nbsp;<b class="price">{{totalsum}}</b>
-</td>
-</tr>
-        </tfoot>
+            </tfoot>
         </table>
         {{/data}}
 
